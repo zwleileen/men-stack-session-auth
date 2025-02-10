b3f3037ee134e871923546717f7da5a6075899f8
eileen <zwleileen@gmail.com>
2025-02-10 13:54:45 +0800

    feat: added sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,6 +22,29 @@ router.post("/sign-up", async (req, res) => {
   res.send(`Thanks for signing up ${user.username}`);
 });
 
+router.get("/sign-in", (req, res) => {
+  res.render("auth/sign-in.ejs");
+});
+
+router.post("/sign-in", async (req, res) => {
+  const userInDatabase = await User.findOne({ username: req.body.username });
+  if (!userInDatabase) {
+    return res.send("Login failed. Please try again.");
+  }
+  const validPassword = bcrypt.compareSync(
+    req.body.password,
+    userInDatabase.password
+  );
+  if (!validPassword) {
+    return res.send("Login failed. Please try again.");
+  }
+  res.redirect("/");
+});
+
+router.post("/sign-in", async (req, res) => {
+  res.send("Request to sign in received!");
+});
+
 // validation logic
 
 // const user = await User.create(req.body);
